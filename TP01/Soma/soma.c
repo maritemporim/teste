b9829b9832eac91740899e0fdf871182297c54d5
Mariana Temporim Ferreira <marianatemporim2007@gmail.com>
2026-08-27 08:16:08 -0300

    tp01

--- TP01/Soma/soma.c
+++ TP01/Soma/soma.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-int main(void){
+int main(void) {
     
+    int n;
+    int soma = 0;
+
+    while(scanf("%d", &n) != EOF){
+        while (n > 0) {
+            soma += n % 10;
+            n /= 10;
+        }
+        printf("%d\n", soma);
+        soma = 0;
+    }
+    return 0;
 }
